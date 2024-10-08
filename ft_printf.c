d80a3ca4e4df031a70d689477267e40299ba83a7
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-10-08 13:11:53 +0200

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,50 +1,46 @@
-#include "../includes/ft_printf.h"
+#include "ft_printf.h"
 
-static void	ft_printf_checker(char s, va_list *args, int *len, int *i)
+static void ft_printf_checker(char s, va_list *args, int *length)
 {
-	if (s == 's')
-		ft_string(va_arg(*args, char *), len);
-	else if (s == 'd' || s == 'i')
-		ft_number(va_arg(*args, int), len);
-	else if (s == 'u')
-		ft_unsigned_int(va_arg(*args, unsigned int), len);
-	else if (s == 'x')
-		ft_hexadecimal(va_arg(*args, unsigned int), len, 'x');
-	else if (s == 'X')
-		ft_hexadecimal(va_arg(*args, unsigned int), len, 'X');
-	else if (s == 'p')
-		ft_pointer(va_arg(*args, size_t), len);
-	else if (s == 'c')
-		ft_putcharacter_length(va_arg(*args, int), len);
-	else if (s == '%')
-		ft_putcharacter_length('%', len);
-	else
-		(*i)--;
+    if (s == 's')
+        ft_string(va_arg(*args, char *), length);
+    else if (s == 'd' || s == 'i')
+        ft_number(va_arg(*args, int), length);
+    else if (s == 'u')
+        ft_unsigned_int(va_arg(*args, unsigned int), length);
+    else if (s == 'x')
+        ft_hexadecimal(va_arg(*args, unsigned int), length, 'x');
+    else if (s == 'X')
+        ft_hexadecimal(va_arg(*args, unsigned int), length, 'X');
+    else if (s == 'p')
+        ft_pointer(va_arg(*args, unsigned long long), length);
+    else if (s == 'c')
+        ft_putcharacter_length(va_arg(*args, int), length);
+    else if (s == '%')
+        ft_putcharacter_length('%', length);
 }
 
-int	ft_printf(const char *string, ...)
+int ft_printf(const char *string, ...)
 {
-	va_list	args;
-	int		i;
-	int		length;
+    va_list args;
+    int i = 0;
+    int length = 0;
 
-	i = 0;
-	length = 0;
-	va_start(args, string);
-	while (string[i] != '\0')
-	{
-		if (string[i] == '%')
-		{
-			i++;
-			ft_printf_checker(string[i], &args, &length, &i);
-			i++;
-		}
-		else
-		{
-			ft_putcharacter_length((char)string[i], &length);
-			i++;
-		}
-	}
-	va_end(args);
-	return (length);
+    va_start(args, string);
+    while (string[i] != '\0')
+    {
+        if (string[i] == '%')
+        {
+            i++;
+            ft_printf_checker(string[i], &args, &length);
+            i++;
+        }
+        else
+        {
+            ft_putcharacter_length(string[i], &length);
+            i++;
+        }
+    }
+    va_end(args);
+    return (length);
 }

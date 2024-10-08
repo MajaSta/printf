c0e46c3ee47b188e3d962a7f10905d05d9e4c57a
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-10-08 13:11:03 +0200

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,19 +1,17 @@
 #ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#define FT_PRINTF_H
 
-# include <stdio.h>
-# include <stdarg.h>
-# include <stddef.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-void	ft_putcharacter_length(char character, int *length);
-void	ft_string(char *args, int *length);
-void	ft_number(int number, int *length);
-void	ft_hexadecimal(unsigned int x, int *length, char x_or_x);
-void	ft_unsigned_int(unsigned int u, int *length);
-void	ft_pointer(unsigned long long pointer, int *length);
+int ft_printf(const char *format, ...);
 
-int		ft_printf(const char *string, ...);
+void ft_putcharacter_length(char character, int *length);
+void ft_string(char *str, int *length);
+void ft_number(int number, int *length);
+void ft_unsigned_int(unsigned int u, int *length);
+void ft_hexadecimal(unsigned int x, int *length, char x_or_x);
+void ft_pointer(unsigned long long pointer, int *length);
 
 #endif
+

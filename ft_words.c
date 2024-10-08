015f699bc011bfc387d051e77bc2b4e92b88d6c9
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-10-08 13:13:26 +0200

    Update ft_words.c

--- ft_words.c
+++ ft_words.c
@@ -1,25 +1,23 @@
-#include "../includes/ft_printf.h"
+#include "ft_printf.h"
 
-void	ft_putcharacter_length(char character, int *length)
+void ft_putcharacter_length(char character, int *length)
 {
-	write(1, &character, 1);
-	(*length)++;
+    write(1, &character, 1);
+    (*length)++;
 }
 
-void	ft_string(char *str, int *length)
+void ft_string(char *str, int *length)
 {
-	size_t		i;
-
-	i = 0;
-	if (!str)
-	{
-		write(1, "(null)", 6);
-		(*length) += 6;
-		return ;
-	}
-	while (str[i] != '\0')
-	{
-		ft_putcharacter_length(str[i], length);
-		i++;
-	}
+    size_t i = 0;
+    if (!str)
+    {
+        write(1, "(null)", 6);
+        (*length) += 6;
+        return;
+    }
+    while (str[i] != '\0')
+    {
+        ft_putcharacter_length(str[i], length);
+        i++;
+    }
 }
